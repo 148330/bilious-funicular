49401f5e3ced8889811aa3b2e327bf25f837b72b
unknown <148330@WKS-HP-1725.ASG.LOCAL>
2015-12-07 16:29:42 +0100

    no message

--- oefening.php
+++ oefening.php
@@ -1,11 +1,2 @@
 oefening nummer 1
-hahahahaha hoi iris heel grappig
-hoi!!!!
-
-dit is de opdracht Iris, fijn dat je er bij bent
-
-Jap
-
-goed, laten we beginnen
-
-IK BEN ONDERAAN. POEP KOMT OOK VAN ONDERAAN
+en nu serieus
