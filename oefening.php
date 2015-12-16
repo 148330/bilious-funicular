11cd78cd7e7a366bd43c3ad3438537b376ca380e
Casper Schulte <148328@helenpark.nl>
2015-12-16 13:18:05 +0100

    yoloswag

--- oefening.php
+++ oefening.php
@@ -1,4 +1,1 @@
-
-hier gaan we zo alledrie werken IIRIISIISISIISIRIIIDISI
-
-ik vind het helemaal mooi hier gaan we zo alledrie werken(ik ben casper)
+ik vind het helemaal mooi hier gaan we zo alledrie werken(ik ben casper)IIRIISIISISIISIRIIIDISI
