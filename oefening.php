38bdbeed1add844a63eb40492cbe697b17c0f8dd
Casper Schulte <148328@helenpark.nl>
2015-12-16 13:27:02 +0100

    casper3

--- oefening.php
+++ oefening.php
@@ -1,14 +1,33 @@
-ik vind het helemaal mooi hier gaan we zo alledrie werken(ik ben casper)IIRIISIISISIISIRIIIDISI
+ik vind het helemaal mozoi hier gaankfdbjhkdfzblfzdbhlfzbdhjlkbfdzlhk;jzbfdh;ojkgfdzhhfdnnlkvbc nmbfxznlbvxzclknlhkbvx we zo alledrie werken(ik ben casper)IIRIISIISISIISIRIIIDISI
 
 
 PFFFFFFFFFFFFF jeetje zeg
 
 yolosagkjasdf[oiausdfoijasdfoiuasdflkjdsfksdff]
 
-asdfoiasdfjenneickfjhsa';oidfgua'dihf;jashdfjhsadjfhasdfyueawiruoewiru
+asdfoiasdfjenneickfjhsa';oidfgua'dihf;skj\osif;ghoiah\sfdblkh\sbrlkhfsalkjhfsglkzhlkgafhlkzsdhvlks\fdbhklfsg
 ik ben heel erg cool met dit en dat en dieso en dasso
 
 
-VERY INTERESTING
+VERY INTERESTING2w'oim;arhgfawero;gh j;alwrjhgk;wnt4cyhotujblkg je5/lkgubx34/l5g.ik4w2un{GP#!ujgh uergj nvw}oijgo;irzshjgibsd\vkj\sdhb;kvnoks\dnf obkjhs\fiobk\sd\cl'd\sfb;kncxvk;bgc
 
 asdkjfhaisdhfkjhasdkjfhkjashdfkjhadkljsfh;iuashdf;iuhwqeiuarhfiujawehriuhwe;oirhfojasdhfkjhasiufrhiuwehfiusahdfiu
+
+
+
+
+
+
+hdjhfdogfodsid93iof,  z;áp[29838\axzs5vgb6hnjjjjjjjjjjj=~|@#qZ45EC6VT7Y8UNM9IO,.P[\]0'[9/P;8.OL7,I6MUJ5NHY4BGT3VFDCZXCVBNM,./
+\';LKJHGFDSA	qwertyuiop[]=-098765432195156161854154665456kjf651ik6j51fd5682if486i819fr01852i0185691855555555555555555555555555555555555555555555555555555550
+
+
+
+
+
+451025610]Q
+
+
+
+
+;ily\awrf natcasesortqierpoawEUPIGFH qi t[wairug[pi mWAPETUV[OAEUTF[PGKUAC 3[OIRUJSEDCAPFTPFOVILQUK3904TVUQOI3UYBOIV3YU04INB=8]
