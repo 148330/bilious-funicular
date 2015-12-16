e01369dff0630321ca0b0d6831830c3a3a3cb7c6
Casper Schulte <148328@helenpark.nl>
2015-12-16 13:20:57 +0100

    ik1

--- oefening.php
+++ oefening.php
@@ -1,1 +1,6 @@
 ik vind het helemaal mooi hier gaan we zo alledrie werken(ik ben casper)IIRIISIISISIISIRIIIDISI
+
+yolosagkjasdf[oiausdfoijasdfoiuasdflkjdsfksdff]
+
+asdfoiasdfjenneickfjhsa';oidfgua'dihf;jashdfjhsadjfhasdfyueawiruoewiru
+ik ben heel erg cool met dit en dat en dieso en dasso
