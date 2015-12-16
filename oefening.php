b0a4f8ae37655601bffdd63aaafeb68d05191428
Casper Schulte <148328@helenpark.nl>
2015-12-16 13:30:36 +0100

    CASPER4

--- oefening.php
+++ oefening.php
@@ -9,7 +9,7 @@ asdfoiasdfjenneickfjhsa';oidfgua'dihf;skj\osif;ghoiah\sfdblkh\sbrlkhfsalkjhfsglk
 ik ben heel erg cool met dit en dat en dieso en dasso
 
 
-VERY INTERESTING2w'oim;arhgfawero;gh j;alwrjhgk;wnt4cyhotujblkg je5/lkgubx34/l5g.ik4w2un{GP#!ujgh uergj nvw}oijgo;irzshjgibsd\vkj\sdhb;kvnoks\dnf obkjhs\fiobk\sd\cl'd\sfb;kncxvk;bgc
+VERY INTERESTING2w'oim;arhgfawero;gh j;alwrjhgk;wnt4cyhotujblkg jeKJBSDF\KJBSGRJKHSFGDHHFDGIUIUFSGKJHXCVN,MNFBDLKHXVCBLKJHFSKVIFD5/lkgubx34/l5g.ik4w2un{GP#!ujgh uergj nvw}oijgo;irzshjgibsd\vkj\sdhb;kvnoks\dnf obkjhs\fiobk\sd\cl'd\sfb;kncxvk;bgc
 
 asdkjfhaisdhfkjhasdkjfhkjashdfkjhadkljsfh;iuashdf;iuhwqeiuarhfiujawehriuhwe;oirhfojasdhfkjhasiufrhiuwehfiusahdfiu
 
@@ -17,17 +17,22 @@ asdkjfhaisdhfkjhasdkjfhkjashdfkjhadkljsfh;iuashdf;iuhwqeiuarhfiujawehriuhwe;oirh
 
 
 
-
+EDK.UHFDSAKJ.HBDSG\KJHDGSA\KJHDSVOIHUEW|IOH'GERS\lKBLIKHVDAOIHOIGEHALKNGERSOIHXZ 
 hdjhfdogfodsid93iof,  z;áp[29838\axzs5vgb6hnjjjjjjjjjjj=~|@#qZ45EC6VT7Y8UNM9IO,.P[\]0'[9/P;8.OL7,I6MUJ5NHY4BGT3VFDCZXCVBNM,./
 \';LKJHGFDSA	qwertyuiop[]=-098765432195156161854154665456kjf651ik6j51fd5682if486i819fr01852i0185691855555555555555555555555555555555555555555555555555555550
 
 
 
-
+DZAVLKDAFLKKLDSV
 
 451025610]Q
 
+SAcfLKNDAClKJNLKCS\;LKJNZDVLKN'FDSGJ?IJESRPOIJ'FR\SGJOI'JSDgvK;LSDGV
+
+
+;ily\awrf natcasesortqierpoawEUPIGFH qi t[wairug[pi mWAPEa\dgkjs\dgvkjdsvalkjkwfajLBKJAD\VKJDAVBJDSVBBDAV
+TUV[OAEUTF[PGKUAC 3[OIRUJSEDCAPFTPFOVILQUK3904TVUQOI3UYBOIV3YU04INB=8]
 
 
 
-;ily\awrf natcasesortqierpoawEUPIGFH qi t[wairug[pi mWAPETUV[OAEUTF[PGKUAC 3[OIRUJSEDCAPFTPFOVILQUK3904TVUQOI3UYBOIV3YU04INB=8]
+zedflkjhsdgf\kjo;hiudgoh;udsg\ojh;d\a;jhikh\sdg;uh;oihdaf
