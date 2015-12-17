c6fa6cf0ca62a4251c03918837c217ce5f4fd19d
unknown <148330@WKS-HP-1743.ASG.LOCAL>
2015-12-17 12:00:02 +0100

    no message

--- oefening.php
+++ oefening.php
@@ -1,7 +1,26 @@
 <?php
 
-
 $bestand = "invoer1.txt";
 $x = file($bestand);
 
-print_r($x);
+//print_r($x);
+$result = array();
+foreach ($x as $line) {
+    $y = preg_split('/\W/', $line);
+    //print_r($y);
+    foreach ($y as $w) {
+        if ($w != '') {
+            $result[] = $w;
+        }
+    }
+}
+print_r($result);
+function mijnHash($w) {
+    return strlen($w);
+}
+$collision = array();
+foreach($result as $w) {
+    $h = mijnHash($w);
+    $collision[$h] = $w;
+}
+print_r($collision);
