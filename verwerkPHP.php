afe022113185dba9afe85766350f6aeffe01ed04
unknown <129878@WKS-HP-1746.ASG.LOCAL>
2016-01-19 12:18:44 +0100

    Fgets test 3

--- verwerkPHP.php
+++ verwerkPHP.php
@@ -1,15 +1,10 @@
 <?php
     $file = $_POST["opdracht"];
     $fh = fopen($file, 'r');
-    $data = fgets($fh). "<br />";
-    fclose($fh);
-    echo $data;
-//$bestand = "invoer1.txt";
-//$x = file($bestand);
-//printf($x);     
-//if(isset($_POST["verzend"]))
-//{
-//$file = $_POST['opdracht'];
-//}
-           
+    $data = fgets($fh);
+    while(! feof($data))
+        {
+        echo fgets($data). "<br />";
+        }
+    fclose($fh);     
 ?>
