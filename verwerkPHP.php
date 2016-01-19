289b05ea2cd36c278eb3bb5aec5b68d1b3fe0034
unknown <148330@WKS-HP-1747.ASG.LOCAL>
2016-01-19 11:36:55 +0100

    no message

--- verwerkPHP.php
+++ verwerkPHP.php
@@ -1,9 +1,8 @@
 <?php
-
-    $bestand = "invoer1.txt";
-    $x = file($bestand);
-    printf($x);     
-
+    $file = $_POST["opdracht"];
+//$bestand = "invoer1.txt";
+//$x = file($bestand);
+//printf($x);     
 //if(isset($_POST["verzend"]))
 //{
 //$file = $_POST['opdracht'];
