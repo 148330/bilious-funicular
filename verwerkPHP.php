edb328e3eef2dacf6a71f064f1835aaad094908c
unknown <148330@WKS-HP-1747.ASG.LOCAL>
2016-01-19 11:29:24 +0100

    no message

--- verwerkPHP.php
+++ verwerkPHP.php
@@ -1,17 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
     $bestand = "invoer1.txt";
     $x = file($bestand);
-    printf($x)     
+    printf($x);     
 
 //if(isset($_POST["verzend"]))
 //{
 //$file = $_POST['opdracht'];
 //}
            
-?>
