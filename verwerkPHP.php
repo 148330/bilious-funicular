4c0e7f3667e5301e708f7688dba2198e86942cef
unknown <148330@WKS-HP-1747.ASG.LOCAL>
2016-01-19 11:23:41 +0100

    no message

--- verwerkPHP.php
+++ verwerkPHP.php
@@ -5,12 +5,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+    $bestand = "invoer1.txt";
+    $x = file($bestand);
+    printf($x)     
 
-    $file = $_POST['opdracht'];
-        
-
-           if(isset($_POST["verzend"]))
-           {
-            $file = $_POST['opdracht'];
-           }
+//if(isset($_POST["verzend"]))
+//{
+//$file = $_POST['opdracht'];
+//}
            
+?>
