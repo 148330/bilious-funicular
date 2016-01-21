b470171205559a895b6077a69339ec444bc8ac56
unknown <148330@WKS-HP-1748.ASG.LOCAL>
2016-01-21 12:21:13 +0100

    nog mee bezig

--- leesBestand.php
+++ leesBestand.php
@@ -1,12 +1,21 @@
 <?php
-    $file_handle = fopen("$file", "r");
-        while (!feof($file_handle)) 
-            {
-            $line = fgets($file_handle);
-            echo $line. "<br />";
-            }
-    fclose($file_handle);
 
-    $ruweWoordzoeker = Array(); // van de regels
-    $woorden = Array(); // woorden
+$ruweWoordzoeker = Array(); // van de regels
+$woorden = Array(); // woorden
+$file_handle = fopen("$file", "r");
+while (!feof($file_handle)) {
+    $line = fgets($file_handle);
+    if (strlen($line) <= 2) {
+        // nu komen de woorden
+        continue;
+    }
+    $ruweWoordzoeker[] = $line; // append 
+    $woorden[] = $line;
+    //echo $line. "<br />";
+}
+fclose($file_handle);
+
+print "<pre>";
+print_r($ruweWoordzoeker);
+print_r($woorden);
 
