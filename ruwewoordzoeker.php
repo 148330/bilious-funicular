fed5ade0a1de80ddcc5e4817e3fdf24140ad9a4b
unknown <148330@WKS-HP-1747.ASG.LOCAL>
2016-01-26 11:38:11 +0100

    Cas|3er

--- ruwewoordzoeker.php
+++ ruwewoordzoeker.php
@@ -3,9 +3,9 @@
 $letter = "abcdefghijklmnopqrstuvwxyz";
 
 $regel = array('-', 'w');
-foreach($regel as $i => $w) {
-if ($w == '-')(
+foreach($regel as $i => $w){
+        if ($w == '-'){
                 $l = rand(0,26);
                 $regel [$i] = $letters [$l];
-         )
+        }
 }
