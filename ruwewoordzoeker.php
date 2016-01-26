d47021ce1836bf45a70bb165f42ba2cc2c8eb0d6
unknown <148330@WKS-HP-1747.ASG.LOCAL>
2016-01-26 12:12:17 +0100

    proberen

--- ruwewoordzoeker.php
+++ ruwewoordzoeker.php
@@ -1,5 +1,5 @@
 <?php
 
 $letters = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-
-str_replace("-","mixed array_rand(array $letters[, int $num = 1] )","$ruweWoordzoeker");
+$random_keys = array_rand($letters,1);
+str_replace("-","$random_keys","$ruweWoordzoeker");
