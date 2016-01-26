c2d416756f6b89e1191c4ccde7d0060134be6737
unknown <129878@WKS-HP-1746.ASG.LOCAL>
2016-01-26 11:36:44 +0100

    Cas|)er

--- ruwewoordzoeker.php
+++ ruwewoordzoeker.php
@@ -1,10 +1,11 @@
 <?php
-&letter = "abcdefghijklmnopqrstuvwxyz";
 
-&regel = array('-', 'w');
-foreach(&regel as &i => &w) (
-        if (&w == '-')(
-                &l = rand(0,26) 
-                &regel [&i] = &letters [&l]
+$letter = "abcdefghijklmnopqrstuvwxyz";
+
+$regel = array('-', 'w');
+foreach($regel as $i => $w) {
+if ($w == '-')(
+                $l = rand(0,26);
+                $regel [$i] = $letters [$l];
          )
- )
+}
