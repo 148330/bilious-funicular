4401f83812895f771ba081a005ad0b21cada3173
unknown <129878@WKS-HP-1746.ASG.LOCAL>
2016-01-26 12:09:48 +0100

    Elia Martell

--- ruwewoordzoeker.php
+++ ruwewoordzoeker.php
@@ -2,4 +2,4 @@
 
 $letters = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
 
-str_replace('-','mixed array_rand(array $letters[, int $num = 1] )','$ruweWoordzoeker');
+str_replace('-','mixed array_rand($letters[, int $letters = 1] )','$ruweWoordzoeker');
