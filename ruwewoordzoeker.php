5b865e51ffcb9f8be8fa3c6a3852a8d235c661bd
unknown <129878@WKS-HP-1746.ASG.LOCAL>
2016-01-26 12:18:20 +0100

    Mountain

--- ruwewoordzoeker.php
+++ ruwewoordzoeker.php
@@ -3,4 +3,4 @@
 $letters = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
 
 $random_keys = array_rand($letters,1);
-str_replace("-","$random_keys","$ruweWoordzoeker");
+str_replace($random_keys,"-",$ruweWoordzoeker);
