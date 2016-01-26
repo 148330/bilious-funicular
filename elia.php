17eccaa3377d83ff85c1cc9f0dd7d20e5fed0ab9
unknown <148330@WKS-HP-1747.ASG.LOCAL>
2016-01-26 11:40:14 +0100

    geinclude

--- elia.php
+++ elia.php
@@ -4,6 +4,7 @@
 
 $file = "opdracht4.txt";
 include "leesBestand.php";
+include "ruwewoordzoeker.php";
 
 // include "veranderRuweWoordzoekerNaar2DArray.php";   // lever #wz[][]
 // include "findHorizontaleNaarRechts.php";
