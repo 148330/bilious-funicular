b7f001f37af0f8c59b348156f816fff7a0a10c75
unknown <148330@WKS-HP-1748.ASG.LOCAL>
2016-01-28 12:20:38 +0100

    nog niet klaar

--- ruwewoordzoeker.php
+++ ruwewoordzoeker.php
@@ -3,4 +3,6 @@
 $letters = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
 
 $random_keys = array_rand($letters,1);
-str_replace($random_keys,"-",$ruweWoordzoeker);
+echo "<br>"; 
+echo $random_keys;
+//preg_replace("-",$random_keys, $rw, [1]);
