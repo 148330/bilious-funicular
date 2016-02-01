81e8c252a70e590ea7991820d6097857eab14fa0
unknown <148330@WKS-HP-1746.ASG.LOCAL>
2016-02-01 16:10:38 +0100

    nog niet zo goed

--- ruwewoordzoeker.php
+++ ruwewoordzoeker.php
@@ -2,7 +2,8 @@
 
 $letters = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
 
-$random_keys = array_rand($letters,1);
+$random_letter = array_rand($letters,1);
 echo "<br>"; 
-echo $random_keys;
-//preg_replace("-",$random_keys, $rw, [1]);
+echo $random_letter;
+$rw = preg_replace("/-+/",$random_letter, $rw);
+$rw = str_replace("-","$random_letter","$rw");
