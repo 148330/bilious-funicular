c3809c0fe53aab484ad94d5b634dd1e4984d199f
Elia Samuel <148330@hpleerling.nl>
2016-02-02 12:19:52 +0100

    bezig met tabel

--- op_volgorde.php
+++ op_volgorde.php
@@ -1,4 +1,14 @@
 <?php
-
+//$q= str_split(implode('', $oWoord));
+//$oWoord = array();
+//$tabel = array();
+//foreach ($q as $leter) {
+    //if ($leter == $letters) {
+      //  $tabel = "<td>"$leter"<td/>";
+    //} else
+  //  if ($q == "<br>") {
+  //      $tabel = "<tr/><tr>"; 
+  //  }
+//}
 print implode('', $oWoord); echo "<br><br>";
 print_r($w);
