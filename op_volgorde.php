08a07d28b92863a914ba1f53cb82e9539bbb0ee9
Elia Samuel <148330@hpleerling.nl>
2016-02-04 12:30:46 +0100

    WERKEND!!

--- op_volgorde.php
+++ op_volgorde.php
@@ -1,18 +1,21 @@
 <?php
 $letters = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
 echo "<table border='1px solid black' style='width: 100%' height='20px'>";
+foreach ($ruweWoordzoeker as $rij) 
+{
     echo "<tr>";
-        foreach ($oWoord as $r) {
-            //if ($r == $letters){
-                echo "<td>".$r."</td>";
-            //}
-            //else{
-            //    echo "</tr>"."<tr>";
-            //}
-        }
+    foreach ($rij as $letter)
+    {
+        echo "<td>" . $letter . "</td>";
+    }
     echo "</tr>";
+}
 echo "</table>";
 
 //print implode('', $oWoord); 
 echo "<br>";
-print_r($w);
+
+foreach ($woorden as $woord)
+{
+    echo "<div>" . $woord . "</div>";
+}
