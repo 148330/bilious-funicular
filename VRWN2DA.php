8910c4cbae617306bb0f764cdcdd49dedf931bb5
unknown <148328@WKS-HP-1723.ASG.LOCAL>
2016-02-10 14:05:12 +0100

    hover

--- VRWN2DA.php
+++ VRWN2DA.php
@@ -1,6 +1,19 @@
 <html>
     <head>
-        
+       <script src="jquery-1.12.0.min.js">
+       </script> 
+       <script>
+       $(document).Ready(function()){
+           <?php
+           foreach ($woorden as $woord) {
+             generatehovercode($woord);
+               ${'#woord'}. mousein(function(){
+                 ${'.woord'}.css(color: red);  
+               });
+           }
+           ?>
+       });
+       </script>
     </head>
         <body>
 
