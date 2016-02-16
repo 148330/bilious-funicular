244b51ead3b6b1da66b2d5e65b88aff1e502f77f
unknown <148330@WKS-HP-1723.ASG.LOCAL>
2016-02-16 12:18:14 +0100

    hover gelukt

--- elia.php
+++ elia.php
@@ -15,25 +15,39 @@
             $(document).ready(function()
             {
                 console.log("1");
-                $("div.woord").hover(function()
-                {
-                    var block = $(this);
-                    console.log(block.css("background-color"));
-                    if(block.css("background-color") != "rgb(255, 0, 0)")
-                        block.css("background-color", "red");
-                    else
-                        block.css("background-color", "white");
+                
+                $("div.woord").click(function() {
+                    $(this).unbind('mouseout');  
+                }).mouseover(function() {
+                    $(this).css("background-color", "red");
+                }).mouseout(function() {
+                    $(this).css("background-color", "white");
                 });
-                $("div.woord").click(function()
-                {
-                    var block = $(this);
-                    console.log(block.css("background-color"));
-                    if(block.css("background-color") != "rgb(255, 0, 0)")
-                        block.css("background-color", "red");
-                    else
-                        block.css("background-color", "white");
-                });
+                //$("div.woord").hover(function()
+                //{
+                 //   
+                //    console.log(block.css("background-color"));
+                //    if(block.css("background-color") != "rgb(255, 0, 0)")
+                //        block.css("background-color", "red");
+                //    else
+                //        block.css("background-color", "white");
+                //});
+                //$("div.woord").click(function()
+                //{
+                //    var block = $(this);
+                 //   console.log(block.css("background-color"));
+                //    if(block.css("background-color") != "rgb(0, 255, 0)")
+                //        block.css("background-color", "green");
+
+//                });
             });
     </script>
     </body>
 </html>
+$('#foo').click(function() {
+    $(this).unbind('mouseout');  
+}).mouseover(function() {
+    bar.show();
+}).mouseout(function() {
+    bar.hide();
+});
