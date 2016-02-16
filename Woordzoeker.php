c88ff1af99a8409cf1f1af4960b644a220de64f9
unknown <148330@WKS-HP-1723.ASG.LOCAL>
2016-02-16 11:24:21 +0100

    fouten eruit gehaald

--- Woordzoeker.php
+++ Woordzoeker.php
@@ -1,7 +1,4 @@
 <?php
 
-<<<<<<< HEAD
 str_replace("-",$letter,$ruweWoordzoeker);
-=======
-str_replace("-","$letter","$ruweWoordzoeker");
->>>>>>> 62c9161e5a0c126704d04f491a9fb9d6d51136b9
+
--- verwerkPHP.php
+++ verwerkPHP.php
@@ -3,12 +3,9 @@
 //print_r($_POST);
     $file = $_POST['opdracht'];
     include "leesBestand.php";
-<<<<<<< HEAD
-     
-    
-=======
+
     include "ruwewoordzoeker.php";
     include "op_volgorde.php";
     include "VRWN2DA.php";
 ?>
->>>>>>> 62c9161e5a0c126704d04f491a9fb9d6d51136b9
+
