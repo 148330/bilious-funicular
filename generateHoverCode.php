e52e36eb0681e1a1560f3004107d421377985b99
JBN <jbunscho@helenpark.nl>
2016-02-20 10:37:35 +0100

    generateHoverCode genereert nu unieke code voor elk woord. . Er is nog geen unit test voor geschreven

--- generateHoverCode.php
+++ generateHoverCode.php
@@ -1,5 +1,9 @@
 
 <?php
 function generateHoverCode($woord) {
-    echo '${\'#woord\'}.mousein(function() { ${\'.woord\'}.css(\'color\', \'red\');    });';
+    // generate code for each word
+    // if on id=$woord iets gedaan wordt, dan class=$woord wordt aangepast
+    // stel $woord is abc dan
+    // $('#abc').mousein(function() { $('.abc').css('color': 'red') });
+    echo '${\'#$woord\'}.mousein(function() { ${\'.$woord\'}.css(\'color\', \'red\');    });';
 }
