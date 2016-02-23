f6cff822ea060ea838ec05cd787e967d5b796a67
Elia Samuel <148330@hpleerling.nl>
2016-02-23 15:28:10 +0100

    layout soort van goed

--- op_volgorde.php
+++ op_volgorde.php
@@ -1,7 +1,7 @@
 <?php
 
 $letters = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-echo "<table border='1px solid black' style='width: 55%' 'height= 100%'>";
+echo "<table border='1px solid black'>";
 foreach ($ruweWoordzoeker as $x => $rij) {
     echo "<tr>";
     foreach ($rij as $y => $letter) {
@@ -10,7 +10,7 @@ foreach ($ruweWoordzoeker as $x => $rij) {
         if (isset($classInformatie[$x][$y])) {
             $classInfo = "class='" . implode(' ', $classInformatie[$x][$y]) . "'";
         }
-        echo "<td $classInfo>" . $letter . "</td>";
+        echo "<td style='height: 25px; width: 25px;'$classInfo>" . $letter . "</td>";
     }
     echo "</tr>";
 }
