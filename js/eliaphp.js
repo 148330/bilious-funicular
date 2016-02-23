5b70561d75b1b8a684ce5b81db72b65059423268
Casper Schulte <148328@hpleerling.nl>
2016-02-23 11:40:41 +0100

    hover color blauw

--- js/eliaphp.js
+++ js/eliaphp.js
@@ -18,7 +18,7 @@ $(document).ready(function ()
 {
     
     console.log("1");
-    include "generateHoverCode.php";
+    include "../generateHoverCode.php";
     foreach($woorden as $woord)     
         generateHoverCode($woord);
     /*
