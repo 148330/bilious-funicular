5ebb3e8e01d2417248992238207ebbeb86a3ec0a
Casper Schulte <148328@hpleerling.nl>
2016-02-23 11:34:20 +0100

    verbetering casper

--- js/casper.php
+++ js/casper.php
@@ -5,7 +5,7 @@ $(document).ready(function ()
     
     console.log("1");
     <?php
-    include "../generateHoverCode.php";
+    include "generateHoverCode.php";
     foreach($woorden as $woord)    { 
         generateHoverCode($woord);
         
