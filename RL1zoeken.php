2198e57d2669e6ed647ce7691deec926c7a11757
unknown <148330@WKS-HP-1723.ASG.LOCAL>
2016-02-23 09:23:17 +0100

    RL1zoeken.php

---
+++ RL1zoeken.php
@@ -0,0 +1,45 @@
+<?php
+ 
+include_once "nietMeerOpGrid.php";
+foreach($woorden as $w) 
+{
+    RL1zoeken($ruweWoordzoeker, $w);
+}
+    function RL1zoeken(Array $ruweWoordzoeker, $w) 
+        {
+        global $classInformatie;
+        $woordArray = str_split(trim($w));
+        foreach($ruweWoordzoeker as $x => $rij) 
+            {
+            foreach($rij as $y => $letter) 
+                {
+                if ($letter == $woordArray[0]) 
+                    {
+                    if (isHetRL1woordDaar($x, $y, $ruweWoordzoeker, $woordArray)) 
+                        {
+                        //regirsteer class
+                        //Je wilt nu het zoeken linken met waar je met je muis 
+                        //over heen gaat, dit doe je door when mouse over word..
+                        //then "voer dit bestand uit" en kleur dit woord.
+                        foreach($woordArray as $k => $letter)
+                            {
+                            $classInformatie[$x + $k][$y - $k][] = $w;
+                            }
+                        }
+                    }
+                }
+            }        
+        } 
+    function isHetRL1woordDaar($x, $y, $ruweWoordzoeker, $woordArray) 
+        {
+            foreach($woordArray as $k => $letter) 
+                {
+                if (nietMeerOpGrid($ruweWoordzoeker, $x + $k, $y - $k)) 
+                    {
+                    return false;
+                    }
+                if ($letter != $ruweWoordzoeker[$x + $k][$y - $k])
+                    return false;
+                }
+            return true;
+        }
